218fabd46af2fb7cc5a42656fe4f98b9e2bcd945
T5ssp <134216175+T5ssp@users.noreply.github.com>
2026-06-06 15:52:36 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBMDwwgiZ1i7jgysd3ez3Pnn7DgT80LGd8",
-  authDomain: "mandoos-store.firebaseapp.com",
-  projectId: "mandoos-store",
-  storageBucket: "mandoos-store.firebasestorage.app",
-  messagingSenderId: "572507274540",
-  appId: "1:572507274540:web:180969443265858c5c0089"
+apiKey: "AIzaSyBMDwwgiZ1i7jgysd3ez3Pnn7DgT80LGd8",
+authDomain: "mandoos-store.firebaseapp.com",
+projectId: "mandoos-store",
+storageBucket: "mandoos-store.firebasestorage.app",
+messagingSenderId: "572507274540",
+appId: "1:572507274540:web:180969443265858c5c0089"
 };
 
 export const firebaseAdminEmails = [
